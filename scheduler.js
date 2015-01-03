f3bf26123e5fbc66773153b18c870c82bc3b65b4
birek <maciej.birecki+github@gmail.com>
2015-01-03 19:54:58 +0100

    data is displayed in JSON format

--- scheduler.js
+++ scheduler.js
@@ -3,46 +3,58 @@ var exec = require('child_process').exec,
     child;
 var app = express()
 
-var output
-function getJSONfromSYSFS(param,callback)
+function getDiskParamValue(path,paramName,callback)
 {
-	exec('cat '+PATH_SYSFS,function(err,stdout,stderr) {
-		properties = stdout.split('\/')
-		schedulersJSON = JSON.stringify(schedulers)
-		callback({properties: { 'name':param,
-				   'scheduler':schedulersJSON } })
-	})
-}
-function getTunablesValues(param,callback)
+        }
+function getDiskParams(disk,path,paramNames,callback)
 {
-	output = []
-	for(i=0;i<param.lenght;i++)
-		getJSONfromSYSFS(param[i],callback)
-}
-function getDeviceDetails(param,callback)
-{
-	PATH_SYSFS="/sys/block/"+param
-	exec('find '+PATH_SYSFS+' -type f -printf "%P\n"',function(err,stdout,stderr)
+	params = []
+	paramNames.forEach(function(paramName)
 	{
-		listOfTunables = stdout.split('\n')	
-		getTunablesValues(listOfTunables,callback)
+		exec('cat '+path+'/'+paramName,function(err,stdout,stderr) {
+			param = { "id":paramName,"value":stdout}
+			params.push(param)
+			if(params.length == paramNames.length)
+				callback(params)
+                })
+	})
+} 
+function getDeviceDetails(devices,diskName,callback)
+{
+	PATH_SYSFS="/sys/block/"+diskName+"/queue"
+	exec('find '+PATH_SYSFS+' -type f -printf "%P "',function(err,stdout,stderr)
+	{
+		disk = { "name":diskName,"params":[] }
+		listOfTunables = stdout.split(' ')	
+			getDiskParams(disk,PATH_SYSFS,listOfTunables,function(params) {
+				console.log("calling cb for adding disk ")
+				disk.params = params
+				callback(disk)
+			})
 	})
 
 }
 function getDevices(param,callback)
 { 
+    devices = []
+
     exec(param,function (error, stdout, stderr) {
-	devices = stdout.split('\n')
-	for(i=0;i<devices.length;i++)
+	disks = stdout.split('\n')
+	for(i=0;i<disks.length;i++)
 	{
-		if(devices[i]!='')
-		getDeviceDetails(devices[i],callback)
+		if(disks[i]!='')
+		{
+			getDeviceDetails(devices,disks[i],function(disk) {
+			console.log(disk)
+			devices.push(disk)
+   			callback(devices)
+			 })
+		}
 	}	
     if (error !== null) {
       console.log('exec error: ' + error);
     }
 })
-
 }
 app.get('/devices', function (req, res) {
   getDevices("lsblk -l -o TYPE,NAME | awk '/disk/  { print $2 }'",function(output){
